a2982767acd38bc771a8e93b1ea7a03e87cb1b66
Mohammed Sadiq Zakariah <68265542+SadiqZak@users.noreply.github.com>
2022-05-08 08:08:13 +0530

    Hero Content mod
    Made changes to the hero content

--- src/Components/Content/HeroContent/HeroContent.jsx
+++ src/Components/Content/HeroContent/HeroContent.jsx
@@ -4,18 +4,28 @@ import "./HeroContent.css";
 const HeroContent = () => {
   return (
     <main className="hero">
-      <div className="hero-header">
-        <h4>Summarize your favourite Podcasts and more with just a click !</h4>
-        <button className="cta-btn-secondary">Upload</button>
-        <input
-          className="hero-input"
-          type="url"
-          name="url-text"
-          id="url-text"
-          placeholder="Paste URL"
-        />
+      <div className="hero-wrapper">
+        <div className="hero-header">
+          <h4>
+            Summarize your favourite Podcasts and more with just a click !
+          </h4>
+        </div>
+        <div className="hero-content-cont">
+          <div className="hero-action-cont">
+            <button className="cta-btn-secondary">Upload</button>
+            <input
+              className="hero-input"
+              type="url"
+              name="url-text"
+              id="url-text"
+              placeholder="Paste URL"
+            />
+          </div>
+          <div>
+            <button className="cta-btn-primary">Summarize</button>
+          </div>
+        </div>
       </div>
-      <button className="cta-btn-primary">Summarize</button>
     </main>
   );
 };
